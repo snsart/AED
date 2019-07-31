7759784639bb742287587dcce7615b57b9df7933
snsart <snsart@163.com>
2019-07-31 15:50:21 +0800

    add

--- test/es6.js
+++ test/es6.js
@@ -1,16 +1,26 @@
 "use strict"
 
-function BubbleSort(arr){
-	for(let i=0,len=arr.length;i<len-1;i++){
-		for(let j=i;j<len;j++){
-			if(arr[i]>arr[j]){
-				[arr[i],arr[j]]=[arr[j],arr[i]];
-			}
+/*
+ *基本形式：
+def boxmullersampling(mu=0, sigma=1, size=1):  
+    u = np.random.uniform(size=size)  
+    v = np.random.uniform(size=size)  
+    z = np.sqrt(-2 * np.log(u)) * np.cos(2 * np.pi * v)  
+    return mu + z * sigma 
+
+ * */
+let nums=[];
+function montecarlo(){
+	while(true){
+		let r1=Math.random();
+		let probability=r1;
+		let r2=Math.random();
+		if(r2<probability){
+			return r1;
 		}
 	}
 }
+for(let i=0;i<100;i++){
+	console.log(montecarlo());
+}
 
-//测试
-let arr=[4,8,2,6,4,7,9,2,13,1];
-BubbleSort(arr);
-console.log(arr);//[1, 2, 2, 4, 4, 6, 7, 8, 9, 13]
