ab32c1a1c5946e2f976ded6e89a9d6b006713847
snsart <snsart@163.com>
2019-08-24 21:56:24 +0800

    add

--- test/es6.js
+++ test/es6.js
@@ -1,40 +1,20 @@
 "use strict"
+/*
+插入排序的工作方式像许多人排序一手扑克牌。开始时，我们左手为空并且桌子上的牌面向下。然后，我们每次从桌子上拿走一张牌并将它插入左手中正确的位置。为了找到一张牌的正确位置，我们从右向左将它与已在手中的每张牌进行比较。拿在左手上的牌总是排好序的，原来这些牌是桌子上牌堆中顶部的牌。*/
 
-function noise(x){
-	x = (x<<13) ^ x;
-	return ( 1.0 - ( (x * (x * x * 15731 + 789221) + 1376312589) &0x7fffffff) / 1073741824.0); 
-}
-
-function smoothedNoise_1(x){
-	return noise(x)/2 + noise(x-1)/4 + noise(x+1)/4
-}
-
-function interpolatedNoise_1(x){
-	let integer_X = Math.floor(x);
-	let fractional_X = x - integer_X;
-	let v1 = smoothedNoise_1(integer_X);
-	let v2 = smoothedNoise_1(integer_X + 1);
-	return cosine_Interpolate(v1 , v2 , fractional_X);
-}
-
-function perlinNoise_1D(x){
-	let total = 0,
-		p = 0.25,//持续度，这里我们使用1/4;
-		n = 4;//叠加4次
-	for(let i=0;i<n;i++){
-		let frequency = Math.pow(2,i);
-		let amplitude = Math.pow(p,i);
-		total = total + interpolatedNoise_1(x * frequency) * amplitude;
+function insertSort(arr){
+	for(let i=1;i<arr.length;i++){
+		let key=arr[i];
+		let k=i-1;
+		while(k>=0&&key<arr[k]){
+			arr[k+1]=arr[k];
+			k--;
+		}
+		arr[k+1]=key;
 	}
-	return total
-}
-//余弦插值函数
-function cosine_Interpolate(a, b, x){
-  let ft = x * 3.1415927
-  let f = (1 - Math.cos(ft)) *0.5
-  return a*(1-f) + b*f
 }
 
-for(var i=0;i<10;i+=0.1){
-	console.log(perlinNoise_1D(i));
-}
+let arr=[4,5,2,1,8,9,4,7,2,3,1,1,6];
+insertSort(arr);
+console.log(arr);
+
